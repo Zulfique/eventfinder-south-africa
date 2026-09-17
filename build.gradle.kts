cf1902c100b1dd54bb0e140e7ba4796a26c345b3
zulfiquej <163821020+zulfiquej@users.noreply.github.com>
2026-09-17 22:53:34 +0200

    chore: initialise Gradle root project scaffolding

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Plugin versions are declared here and applied per-module, keeping dependency versions central.
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+}
